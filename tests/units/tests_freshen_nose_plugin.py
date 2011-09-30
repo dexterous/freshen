9fad4d8a09bd025a9c964f3a531a074035544dd5
Saager Mhatre <saager.mhatre@gmail.com>
2011-09-30 14:48:54 +0530

    More test code refactorings on a whim!
    * make_plugin 'private' method in TestFreshenTestCaseName is more pythonic
    * determining path to resources (read features) is delegated to an appropriately
      named 'private' method

--- tests/units/tests_freshen_nose_plugin.py
+++ tests/units/tests_freshen_nose_plugin.py
@@ -1,18 +1,32 @@
 import unittest
 
-import os
 import sys
 
 from freshen.noseplugin import FreshenNosePlugin
 from optparse import OptionParser
+from os.path import abspath, dirname, join
 
 class TestFreshenTestCaseName(unittest.TestCase):
 
     def __init__(self, method_name='runTest'):
         unittest.TestCase.__init__(self, method_name)
-        self.cur_dir = os.path.dirname(os.path.abspath(__file__))
+        self.cur_dir = dirname(abspath(__file__))
 
-    def _make_plugin(self):
+    def test_should_use_feature_name_as_class_name_when_subclassing_FreshenTestCase(self):
+        plugin = self.__make_plugin()
+        test_generator = plugin.loadTestsFromFile(self.__resource('valid_no_tags_no_use_only'))
+        test_instance = test_generator.next()
+
+        self.assertEquals(test_instance.__class__.__name__, 'Independence of the counter.')
+
+    def test_should_use_scenario_name_as_method_name_when_subclassing_FreshenTestCase(self):
+        plugin = self.__make_plugin()
+        test_generator = plugin.loadTestsFromFile(self.__resource('valid_no_tags_no_use_only'))
+        test_instance = test_generator.next()
+
+        self.assert_(getattr(test_instance, 'Print counter', None) is not None)
+
+    def __make_plugin(self):
         plugin = FreshenNosePlugin()
         parser = OptionParser()
 
@@ -24,16 +38,5 @@ class TestFreshenTestCaseName(unittest.TestCase):
         plugin.configure(options, None)
         return plugin
 
-    def test_should_use_feature_name_as_class_name_when_subclassing_FreshenTestCase(self):
-        plugin = self._make_plugin()
-        test_generator = plugin.loadTestsFromFile(self.cur_dir + '/resources/valid_no_tags_no_use_only.feature')
-        test_instance = test_generator.next()
-
-        self.assertEquals(test_instance.__class__.__name__, 'Independence of the counter.')
-
-    def test_should_use_scenario_name_as_method_name_when_subclassing_FreshenTestCase(self):
-        plugin = self._make_plugin()
-        test_generator = plugin.loadTestsFromFile(self.cur_dir + '/resources/valid_no_tags_no_use_only.feature')
-        test_instance = test_generator.next()
-
-        self.assertNotEqual(getattr(test_instance, 'Print counter', None), None)
+    def __resource(self, feature_file):
+        return join(self.cur_dir, 'resources/%s.feature' % feature_file)
